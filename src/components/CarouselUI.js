17e4a8c8b0a6f726491fc0f9ec6bce19fe5dca8f
soarflat <soarflat777@gmail.com>
2018-12-14 00:57:09 +0900

    refactor: migrate to v6

--- src/components/CarouselUI.js
+++ src/components/CarouselUI.js
@@ -1,12 +1,27 @@
-import { Observable, Subject } from 'rxjs/Rx';
+import { Subject, fromEvent, merge, timer } from 'rxjs';
+import {
+  combineLatest,
+  filter,
+  map,
+  mapTo,
+  mergeMap,
+  take,
+  takeUntil,
+  scan,
+  startWith,
+  switchMap,
+  withLatestFrom,
+} from 'rxjs/operators';
 import anime from 'animejs';
 import { enablePassiveEventListeners } from '../utils/event';
 import { outerWidth } from '../utils/dom';
 import { isMobile } from '../utils/userAgent';
 
+const nextIndex = (index, max) => (index < max ? index + 1 : 0);
+const prevIndex = (index, max) => (index > 0 ? index - 1 : max);
+
 export default class CarouselUI {
   constructor(selector, options = {}) {
-    this.subject = new Subject();
     this.el = document.querySelector(selector);
     this.elWrapper = this.el.querySelector(`${selector}_wrapper`);
     this.elItems = this.el.querySelectorAll(`${selector}_item`);
@@ -23,79 +38,68 @@ export default class CarouselUI {
   bind() {
     const options = enablePassiveEventListeners() ? { passive: true } : false;
 
-    // 各イベントをObservableに変換する
-    const pointerdown$ = Observable.fromEvent(
+    const pointerdown$ = fromEvent(
       this.el,
       isMobile ? 'touchstart' : 'mousedown',
       options
     );
-    const pointermove$ = Observable.fromEvent(
+    const pointermove$ = fromEvent(
       this.el,
       isMobile ? 'touchmove' : 'mousemove',
       options
     );
-    const pointerend$ = Observable.fromEvent(
+    const pointerend$ = fromEvent(
       this.el,
       isMobile ? 'touchend' : 'mouseup',
       options
     );
+    const dragging$ = pointerdown$.pipe(
+      mergeMap(start =>
+        pointermove$.pipe(
+          takeUntil(pointerend$),
+          map(move => move.pageX - start.pageX)
+        )
+      ),
+      map(moveDistance => state => {
+        return { ...state, moveDistance };
+      })
+    );
 
-    // pointerdown$, pointermove$, pointerend$を利用して
-    // ドラッグ中に、「引数のオブジェクトに移動距離をマージする」コールバック関数をemitするObservableを生成する
-    const dragging$ = pointerdown$
-      // `mergeMap`メソッドで、pointerdown中からpointerupするまでのポインタの移動距離をemitするObservableを生成する
-      .mergeMap(start =>
-        // `takeUntil`メソッドで、pointerend$がemitされるまで値をemitするObservableに変換
-        // `map`メソッドでスタート地点からポインタの移動距離をemitするObservableに変換
-        pointermove$
-          .takeUntil(pointerend$)
-          .map(move => move.pageX - start.pageX)
-      )
-      // 「移動距離を引数のオブジェクトにマージする」コールバック関数をemitするObservableを生成する
-      .map(deltaX => state => Object.assign({}, state, { deltaX }));
+    const dragend$ = dragging$.pipe(
+      switchMap(() => pointerend$.pipe(take(1))),
+      withLatestFrom(dragging$),
+      map(([, getState]) => state => {
+        const { moveDistance } = getState();
 
-    // dragging$, pointerend$を利用して
-    // ドラッグ終了後のindexを返すコールバック関数をemitするObservableを生成する
-    const dragend$ = dragging$
-      // pointerend$.take(1)にすることで
-      // switchMapが再び実行されるまで、pointerupイベントが何回発生しても
-      // イベントはemitされなくなる
-      .switchMap(() => pointerend$.take(1))
-      // dragging$ Observableから最後にemitされた値（今回はコールバック関数）を取得
-      .withLatestFrom(dragging$)
-      // dragging$ Observableからemitされたコールバック関数を引数にして
-      // indexを返すコールバック関数をemitするObservableを生成する
-      .map(([e, getState]) => state => {
-        const { deltaX } = getState();
+        let index = state.index;
+        if (moveDistance < -50) index = nextIndex(index, this.maxCount);
+        if (moveDistance > 50) index = prevIndex(index, this.maxCount);
 
-        let index;
-
-        if (deltaX < -50) {
-          index = state.index < this.maxCount ? state.index + 1 : 0;
-        } else if (deltaX > 50) {
-          index = state.index > 0 ? state.index - 1 : this.maxCount;
-        } else {
-          index = state.index;
-        }
-
-        return Object.assign({}, state, { index, deltaX: 0 });
-      });
+        return { ...state, index, moveDistance: 0 };
+      })
+    );
 
     /**
-     * NEXTボタンををクリック時、インデックスを更新し、更新したindexを返すコールバック関数をemitするObservable
+     * NEXTボタンをクリック時、インデックスを更新し、更新したindexを返すコールバック関数をemitするObservable
      */
-    const next$ = Observable.fromEvent(this.elNext, 'click').map(() => state =>
-      Object.assign({}, state, {
-        index: state.index < this.maxCount ? state.index + 1 : 0,
+    const next$ = fromEvent(this.elNext, 'click').pipe(
+      map(() => state => {
+        return {
+          ...state,
+          index: nextIndex(state.index, this.maxCount),
+        };
       })
     );
 
     /**
      * PREVボタンををクリック時、インデックスを更新し、更新したindexを返すコールバック関数をemitするObservable
      */
-    const prev$ = Observable.fromEvent(this.elPrev, 'click').map(() => state =>
-      Object.assign({}, state, {
-        index: state.index > 0 ? state.index - 1 : this.maxCount,
+    const prev$ = fromEvent(this.elPrev, 'click').pipe(
+      map(() => state => {
+        return {
+          ...state,
+          index: prevIndex(state.index, this.maxCount),
+        };
       })
     );
 
@@ -103,93 +107,96 @@ export default class CarouselUI {
      * 読み込み完了時、ウィンドウサイズ変更時にスライドのwidthを更新し、
      * 更新したwidthを返すコールバック関数をemitするObservable
      */
-    const update$ = Observable.merge(
-      Observable.fromEvent(window, 'load'),
-      Observable.fromEvent(window, 'resize')
-    ).map(() => state =>
-      Object.assign({}, state, { unitWidth: outerWidth(this.elItems[0]) })
+    const update$ = merge(
+      fromEvent(window, 'load'),
+      fromEvent(window, 'resize')
+    ).pipe(
+      map(() => state => {
+        return { ...state, unitWidth: outerWidth(this.elItems[0]) };
+      })
     );
 
     /**
      * インディケータをクリック時、クリックしたインディケータのインデックスを返すコールバック関数をemitするObservable
      */
-    const indication$ = Observable.fromEvent(this.elDots, 'click').map(
-      el => state =>
-        Object.assign({}, state, {
-          index: parseInt(el.target.dataset.index, 10),
-        })
+    const indication$ = fromEvent(this.elDots, 'click').pipe(
+      map(el => state => {
+        return { ...state, index: parseInt(el.target.dataset.index, 10) };
+      })
     );
 
-    const mousenter$ = Observable.fromEvent(this.el, 'mouseenter', options);
-    const mouseleave$ = Observable.fromEvent(this.el, 'mouseleave', options);
-    const mousenterd$ = Observable.merge(
-      mousenter$.mapTo(true),
-      mouseleave$.mapTo(false)
-    ).startWith(false);
-    const goto$ = this.subject
-      .combineLatest(mousenterd$)
-      .filter(([, mousenterd]) => !mousenterd);
-    const timer$ = Observable.merge(mouseleave$, goto$)
-      .startWith(true)
-      .switchMap(() =>
-        Observable.timer(3000).takeUntil(
-          Observable.merge(
-            mousenter$,
-            next$,
-            prev$,
-            update$,
-            dragging$,
-            dragend$,
-            indication$
+    const mousenter$ = fromEvent(this.el, 'mouseenter', options);
+    const mouseleave$ = fromEvent(this.el, 'mouseleave', options);
+    const stay$ = merge(
+      mousenter$.pipe(mapTo(true)),
+      mouseleave$.pipe(mapTo(false))
+    ).pipe(startWith(false));
+
+    const subject = new Subject();
+    const goto$ = subject.pipe(
+      combineLatest(stay$),
+      filter(([, mousenterd]) => !mousenterd)
+    );
+
+    const timer$ = merge(mouseleave$, goto$).pipe(
+      startWith(true),
+      switchMap(() =>
+        timer(3000).pipe(
+          takeUntil(
+            merge(
+              mousenter$,
+              next$,
+              prev$,
+              update$,
+              dragging$,
+              dragend$,
+              indication$
+            )
           )
         )
-      )
-      .map(() => state =>
-        Object.assign({}, state, {
+      ),
+      map(() => state => {
+        return {
+          ...state,
           index: state.index < this.maxCount ? state.index + 1 : 0,
-        })
-      );
-
-    Observable.merge(
-      prev$,
-      next$,
-      update$,
-      dragging$,
-      dragend$,
-      indication$,
-      timer$
-    )
-      // state の初期値は`{ deltaX: 0, index: 0, unitWidth: outerWidth(this.elItems[0] }`
-      // `changeFn`には各Observableからemitされたコールバック関数が参照される
-      .scan((state, changeFn) => changeFn(state), {
-        deltaX: 0,
-        index: 0,
-        unitWidth: outerWidth(this.elItems[0]),
+        };
       })
-      .subscribe(({ deltaX, index, unitWidth }) => {
+    );
+
+    merge(prev$, next$, update$, dragging$, dragend$, indication$, timer$)
+      .pipe(
+        // state の初期値は`{ moveDistance: 0, index: 0, unitWidth: outerWidth(this.elItems[0] }`
+        // `changeFn`には各Observableからemitされたコールバック関数が参照される
+        scan((state, changeFn) => changeFn(state), {
+          moveDistance: 0,
+          index: 0,
+          unitWidth: outerWidth(this.elItems[0]),
+        })
+      )
+      .subscribe(({ moveDistance, index, unitWidth }) => {
         this.goTo(
           this.getOptionsForGoto({
-            deltaX,
+            moveDistance,
             index,
             unitWidth,
           })
-        ).then(() => this.subject.next());
+        ).then(() => subject.next());
         this.updateDots(this.elDots, index);
       });
   }
 
   /**
    * カルーセルのスライドを制御するオプションを取得する
-   * @param {number} deltaX
+   * @param {number} moveDistance
    * @param {number} index
    * @param {number} unitWidth
    */
-  getOptionsForGoto({ deltaX, index, unitWidth }) {
-    return deltaX !== 0
+  getOptionsForGoto({ moveDistance, index, unitWidth }) {
+    return moveDistance !== 0
       ? {
           el: this.elWrapper,
           translateX: -unitWidth * index,
-          offset: deltaX,
+          offset: moveDistance,
           duration: 0,
         }
       : { el: this.elWrapper, translateX: -unitWidth * index };
